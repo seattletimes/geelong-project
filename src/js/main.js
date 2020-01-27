acfa2a74c2b4a5ee7b38f5e8af3e53943e7073a2
Hilary Fung <hfung@seattletimes.com>
2020-01-27 15:13:04 -0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(12345678), 5000);
+
+require("waypoints/lib/noframework.waypoints.min");
+
+// once user has scrolled to beginning of story, fade out hero looper
+var beginning = document.querySelector("#beginning"),
+	hero_looper = document.querySelector(".hero__figure");
+
+function animateHero() {
+	new Waypoint({
+	  element: beginning,
+	  offset: "95%",
+	  handler: function(direction) {
+	  	if (direction === "down") {
+	  		hero_looper.classList.remove("shown");
+	  		hero_looper.classList.add("hidden");
+	  	} else {
+	  		hero_looper.classList.remove("hidden");
+	  		hero_looper.classList.add("shown");
+	  	}
+	  }
+	});
+}
+
+animateHero();
